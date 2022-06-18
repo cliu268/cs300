482750e17f0f51ef74b3cb80a09e0cf4332e7cf0
cliu268 <cliu268@gmail.com>
2022-06-17 22:47:41 -0400

    cs300 hw1 q

---
+++ hw1/q3.cpp
@@ -0,0 +1,29 @@
+// Restaurant Customers
+// https://xjoi.net/contest/4359/problem/3
+
+/*
+You are given the arrival and leaving times of n customers in a restaurant.
+What was the maximum number of customers in the restaurant at any time?
+
+Input
+The first input line has an integer n: the number of customers.
+After this, there are n lines that describe the customers. Each line has two integers a and b: 
+the arrival and leaving times of a customer.
+You may assume that all arrival and leaving times are distinct.
+
+Output
+Print one integer: the maximum number of customers.
+
+Constraints
+1≤n≤2⋅105
+1≤a<b≤109
+
+Sample Input:
+3
+5 8
+2 4
+3 9
+
+Sample Output:
+2
+*/
